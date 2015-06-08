0c155b60dff1d804bd9031a1f7128b14fc7b06b5
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-06-08 19:02:17 +0300

    Make sync tests passed

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -268,19 +268,20 @@ class FailingQueue(mixedqueue.Queue):
 
 class FailingQueueTest(BlockingTestMixin, unittest.TestCase):
 
-    def failing_queue_test(self, q):
+    def failing_queue_test(self, _q):
+        q = _q.sync_queue
         if q.qsize():
             raise RuntimeError("Call this function with an empty queue")
         for i in range(QUEUE_SIZE-1):
             q.put(i)
         # Test a failing non-blocking put.
-        q.fail_next_put = True
+        _q.fail_next_put = True
         try:
             q.put("oops", block=0)
             self.fail("The queue didn't fail when it should have")
         except FailingQueueException:
             pass
-        q.fail_next_put = True
+        _q.fail_next_put = True
         try:
             q.put("oops", timeout=0.1)
             self.fail("The queue didn't fail when it should have")
@@ -289,7 +290,7 @@ class FailingQueueTest(BlockingTestMixin, unittest.TestCase):
         q.put("last")
         self.assertTrue(qfull(q), "Queue should be full")
         # Test a failing blocking put
-        q.fail_next_put = True
+        _q.fail_next_put = True
         try:
             self.do_blocking_test(q.put, ("full",), q.get, ())
             self.fail("The queue didn't fail when it should have")
@@ -299,9 +300,12 @@ class FailingQueueTest(BlockingTestMixin, unittest.TestCase):
         # put failed, but get succeeded - re-add
         q.put("last")
         # Test a failing timeout put
-        q.fail_next_put = True
+        _q.fail_next_put = True
         try:
-            self.do_exceptional_blocking_test(q.put, ("full", True, 10), q.get, (),
+            self.do_exceptional_blocking_test(q.put,
+                                              ("full", True, 10),
+                                              q.get,
+                                              (),
                                               FailingQueueException)
             self.fail("The queue didn't fail when it should have")
         except FailingQueueException:
@@ -321,14 +325,14 @@ class FailingQueueTest(BlockingTestMixin, unittest.TestCase):
             q.get()
         self.assertTrue(not q.qsize(), "Queue should be empty")
         q.put("first")
-        q.fail_next_get = True
+        _q.fail_next_get = True
         try:
             q.get()
             self.fail("The queue didn't fail when it should have")
         except FailingQueueException:
             pass
         self.assertTrue(q.qsize(), "Queue should not be empty")
-        q.fail_next_get = True
+        _q.fail_next_get = True
         try:
             q.get(timeout=0.1)
             self.fail("The queue didn't fail when it should have")
@@ -337,7 +341,7 @@ class FailingQueueTest(BlockingTestMixin, unittest.TestCase):
         self.assertTrue(q.qsize(), "Queue should not be empty")
         q.get()
         self.assertTrue(not q.qsize(), "Queue should be empty")
-        q.fail_next_get = True
+        _q.fail_next_get = True
         try:
             self.do_exceptional_blocking_test(q.get, (), q.put, ('empty',),
                                               FailingQueueException)
@@ -352,7 +356,7 @@ class FailingQueueTest(BlockingTestMixin, unittest.TestCase):
     def test_failing_queue(self):
         # Test to make sure a queue is functioning correctly.
         # Done twice to the same instance.
-        q = FailingQueue(QUEUE_SIZE).sync_queue
+        q = FailingQueue(QUEUE_SIZE)
         self.failing_queue_test(q)
         self.failing_queue_test(q)
 
