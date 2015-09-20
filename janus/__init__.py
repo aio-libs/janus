9128177b2926e6d211a7ea61acabbe99bf88186a
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-09-20 05:33:00 +0500

    Support Python 3.5

--- janus/__init__.py
+++ janus/__init__.py
@@ -8,7 +8,7 @@ from heapq import heappop, heappush
 from queue import Empty as SyncQueueEmpty
 from queue import Full as SyncQueueFull
 
-__version__ = '0.1.5'
+__version__ = '0.2.0'
 
 log = logging.getLogger(__package__)
 
--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries'
     ],
     author='Andrew Svetlov',
